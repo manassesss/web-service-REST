289151c57e7485b44b242bb947ddc7286f212f7e
manasses.silva <manasses3mm@gmail.com>
2021-06-11 11:29:43 -0300

    Get my, get my head in the game...

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,16 @@ const http = require('http')
 const emails = require('./data/emails')
 
 const server = http.createServer((req, res) => {
-
-    res.writeHead(200 , {'Constent-Type': 'application/json'})
-    res.end(JSON.stringify(emails))
+    if (req.url === '/api/emails' && req.method === 'GET'){
+        res.writeHead(200 , {'Constent-Type': 'application/json'})
+        res.end(JSON.stringify(emails))
+    }else if(req.url === '/api/users' && req.method === 'GET'){
+        res.writeHead(404 , {'Constent-Type': 'application/json'})
+        res.end(JSON.stringify({message :'Route Not Found'}))
+    }else {
+        res.writeHead(404 , {'Constent-Type': 'application/json'})
+        res.end(JSON.stringify({message :'Route Not Found'}))
+    }
     
 })
 const PORT = process.env.PORT || 1998
